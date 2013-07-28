03026def48a27fd4a2f17149c30d68de4eb85718
Jakub Jankiewicz <jcubic@onet.pl>
2013-07-28 17:15:03 +0200

    Add base cursor movement

--- js/jquery.micro-src.js
+++ js/jquery.micro-src.js
@@ -55,13 +55,59 @@
         this._footer = $('<div/>').addClass('footer').appendTo(this._root);
         this._message = $('<div/>').addClass('message').append('<span/>').
             appendTo(this._root).find('span');
+        this._lines = [''];
+        this._set_cursor(0, 0);
+        var self = this;
+        $(document).bind('keydown.micro', function(e) {
+            if (e.which === 37) { // left
+                self._set_cursor(self._cursor.x-1, self._cursor.y);
+            } else if (e.which === 38) { // top
+                self._set_cursor(self._cursor.x, self._cursor.y-1);
+            } else if (e.which === 39) { // right
+                self._set_cursor(self._cursor.x+1, self._cursor.y);
+            } else if (e.which === 40) { // down
+                self._set_cursor(self._cursor.x, self._cursor.y+1);
+            } else if (e.which === 35) { //end
+                self._set_cursor(self._lines[self._cursor.y].length, self._cursor.y);
+            } else if (e.which === 36) { //home
+                self._set_cursor(0, self._cursor.y);
+            }
+        });
     }
     // -----------------------------------------------------------------------
     micro.prototype = {
+        destroy: function() {
+            this._root.removeData('micro').removeClass('micro');
+            this._table.remove();
+            this._footer.remove();
+            this._message.remove();
+            $(document).unbind('.micro');
+        },
         refresh: function() {
             this._rows = Math.floor(this._root.height() / this._letter.height) - 4;
             this._cols = Math.floor(this._root.width() / this._letter.width);
             return this;
+        },
+        text: function() {
+            return this._lines.join('\n');
+        },
+        _set_cursor: function(x, y) {
+            this._table.find('.cursor').removeClass('cursor');
+            if (x > this._cols) {
+                if (x > this._lines[y].length) {
+                    throw "Can't get that far";
+                } else {
+                    // line continue mode
+                }
+            } else {
+                if (x > this._lines[y].length) {
+                    this._matrix[y][this._lines[y].length].addClass('cursor');
+                } else {
+                    this._matrix[y][x].addClass('cursor');
+                }
+                this._cursor.x = x;
+                this._cursor.y = y;
+            }
         },
         _view: function(offset) {
             if (this._lines) {
